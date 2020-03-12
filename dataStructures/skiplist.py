789db16410b9da446fde5ed1a9d6930033c89de5
Evance Soumaoro <evanxg852000@yahoo.fr>
2020-03-12 13:59:13 +0100

    fixed skiplist

--- dataStructures/skiplist.py
+++ dataStructures/skiplist.py
@@ -1,119 +1,133 @@
+# course: http://www.mathcs.emory.edu/~cheung/Courses/323/
+# http://www.mathcs.emory.edu/~cheung/Courses/323/Syllabus/Map/skip-list.html
 # https://www.cs.cmu.edu/~ckingsf/bioinfo-lectures/skiplists.pdf
-# TODO: needs to check null pointer issues & remove
+import unittest 
 import random
 from datetime import datetime
 
-import unittest
-
 class Node:
-    def __init__(self, height=0, data=(None, None), prev=None):
-        self.next = [None]*height
-        self.key = data[0]
-        self.val = data[1]
+    def __init__(self, key, value, level):
+        self.key = key
+        self.value = value
+        self.next = [None]*level
         self.prev = None
 
 class SkipList:
-    
+
     def __init__(self, maxLevel):
         self.maxLevel = maxLevel
-        self.head = Node(maxLevel)
+        self.head = Node('min-inf', None, maxLevel)
+        self.tail = Node('max-inf', None, maxLevel)
         random.seed(datetime.now())
+        #create links between head->tail
+        for i in range(maxLevel):
+            self.head.next[i] = self.tail
+            self.tail.prev = self.head
 
-    def pickLevel(self):
+    def _pick_level(self):
         height = 1
         while random.randint(1,2) != 1 and height < self.maxLevel:
             height += 1
         return height
 
-    def insert(self, key, val):
-        # find node or lowest level insertion point
-        cn = self._find_terminal(key)
-        if cn.key == key: # update value
-            cn.val = val
-            return
-        
-        # insert node at each level from botom to top in stairs fashion
-        cl = 0
-        node = Node(self.pickLevel(), (key, val))
-        while cl < len(node.next):
-            # update linked list pointers
-            if cn != None:
-                node.next[cl] = cn.next[cl]
-
-            if cn != None and cn.next[cl] != None:
-                cn.next[cl].prev = node
-
-            if cn != None:
-                cn.next[cl] = node
-            
-            # make one stair step up if last height
-            if cn != None and cn.next != None and cl == len(cn.next)-1: 
-                cn = cn.prev
-            cl += 1
-
-    def find(self, key):
-        cn = self._find_terminal(key)
-        if cn.key == key:
-            return cn.val
-        return None
-            
-    def remove(self, key):
-        # find the node
-        cn = self._find_terminal(key)
-        if cn == None:
-            return
-        elif cn.key != key:
-            return
-        
-        # update all level pointers
-        cl = 0
-        while cl < len(cn.next):
-            prev = None
-            nxt = None
-            if cn.next[cl] != None:
-                prev = cn.next[cl].prev
-                nxt = cn.next[cl].next[cl]
-
-            if prev != None:
-                prev.next[cl] = nxt
-            if nxt != None:
-                nxt.prev = prev
-            cl += 1
-        
-        cn = None
-
-
     def _find_terminal(self, key):
-        cl = len(self.head.next) - 1
+        cl = self.maxLevel - 1
         cn = self.head
-        while  cl >= 0 :
-            while cn.next != None and cn.next[cl] != None and cn.next[cl].key < key:
+        while  cl >= 0:
+            if cn.next[cl].key == self.tail.key:
+                cl -=1
+                continue
+            elif cn.next[cl].key <= key:
                 cn = cn.next[cl]
-
-            if cn.next[cl] != None and cn.next[cl].key == key:
-                return cn.next[cl]
+                continue
             else:
                 cl -= 1
         return cn
 
+    def put(self, key, val):
+        # find node or lowest level of insertion point
+        cn = self._find_terminal(key)
+        if cn.key == key: # update key
+            cn.value = val
+            return
+
+        level = self._pick_level()
+        node = Node(key, val, level)
+        cl = 0
+        while cl < level:
+            node.next[cl] = cn.next[cl]
+            cn.next[cl].prev = node
+            node.prev = cn
+            cn.next[cl] = node
+            cl += 1
+        
+    def get(self, key):
+        cn = self._find_terminal(key)
+        if cn.key == key:
+            return cn.value
+        return None
+
+    def remove(self, key):
+        cn = self._find_terminal(key)
+        if cn.key != key:
+            return
+        
+        cl = 0
+        level = len(cn.next)
+        while cl < level:
+            cn.prev.next[cl] = cn.next[cl]
+            cn.next[cl].prev = cn.prev
+            cl += 1
 
 
-class SkipListTest(unittest.TestCase):
-    def test_features(self):
-        skplst = SkipList(12)
-        skplst.insert(1, 'evance')
-        skplst.insert(3, 'alex')
-        skplst.insert(2, 'karim')
-        skplst.insert(5, 'jhon')
+class SkipListTest(unittest.TestCase): 
+  
+    def test_init(self):
+        sk = SkipList(12);
+        self.assertEqual(sk.head.next[0].key, sk.tail.key) 
+        self.assertEqual(sk.tail.prev.key, sk.head.key) 
 
-        self.assertEqual(skplst.find(1), 'evance')
-        self.assertEqual(skplst.find(4), None)
-        self.assertEqual(skplst.find(5), 'jhon')
-        self.assertEqual(skplst.find(3), 'alex')
+    def test_find_terminal(self):
+        sk = SkipList(12);
+        tn = sk._find_terminal('test')
+        self.assertEqual(tn.key, sk.head.key) 
+        
+    def test_put(self):
+        sk = SkipList(12);
+        sk.put('age', 23)
+        inode = sk.head.next[0]
+        self.assertEqual(inode.key, 'age')
+        self.assertEqual(inode.value, 23)
+        self.assertEqual(inode.prev.key, sk.head.key)
+        self.assertEqual(inode.next[0].key, sk.tail.key) 
+        self.assertEqual(inode.key, sk.tail.prev.key)
 
-        skplst.remove(5)
-        self.assertEqual(skplst.find(5), None)
+    def test_get(self):
+        sk = SkipList(12);
+        sk.put('name', 'jhone')
+        sk.put('age', 23)
+        self.assertEqual(sk.get('name'), 'jhone')
+        self.assertEqual(sk.get('age'), 23)
+        self.assertEqual(sk.get('force'), None)
+
+    def test_delete(self):
+        sk = SkipList(12);
+        sk.put('name', 'jhone')
+        sk.put('age', 23)
+        sk.put('color', 'blue')
+        sk.put('force', 98)
+
+        sk.remove('age')
+        self.assertEqual(sk.get('name'), 'jhone')
+        self.assertEqual(sk.get('age'), None)
+        self.assertEqual(sk.get('test'), None)
+        self.assertEqual(sk.get('color'), 'blue')
+
+        sk.remove('color')
+        sk.remove('force')
+        self.assertEqual(sk.get('color'), None)
+        self.assertEqual(sk.get('force'), None)
 
 
-if __name__ == '__main__':
-    unittest.main()
+if __name__ == '__main__': 
+    unittest.main() 
